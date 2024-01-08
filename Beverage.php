1f1537ea1e7f67425416f7008c5f8eafa5876a51
Jonasi-Deetens <jonasideetens@hotmail.com>
2024-01-08 15:48:29 +0100

    Make properties private and create function to change them -excercise 3

--- Beverage.php
+++ Beverage.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 class beverage
 {
-    public string $color;
-    public float $price;
-    public string $temperature;
+    private string $color;
+    private float $price;
+    private string $temperature;
 
     public function __construct(string $color, float $price)
     {
@@ -15,22 +15,23 @@ class beverage
         $this->temperature = "cold";
     }
 
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color)
+    {
+        $this->color = $color;
+    }
+
+    public function getTemperature()
+    {
+        return $this->temperature;
+    }
+
     public function getInfo()
     {
         return "This beverage is $this->temperature and $this->color. For a small price of $this->price euro it can be yours.";
     }
 }
-
-/* EXERCISE 1
-TODO: Create a class beverage.
-TODO: Create the properties color (string), price (float) and temperature (string) and also foresee a construct.
-TODO: Have a default value "cold" in the construct for temperature.
-
-Remember for now we will use properties and methods that can be accessed from everywhere.
-TODO: Make a getInfo function which returns "This beverage is <temperature> and <color>."
-TODO: Instantiate an object which represents cola. Make sure that the color is set to black, the price equals 2 euro and the temperature to cold automatically
- print the getInfo on the screen.
-TODO: Print the temperature on the screen.
-
-USE TYPEHINTING EVERYWHERE!
-*/
--- index.php
+++ index.php
@@ -1,22 +1,26 @@
 <?php
 
-    declare(strict_types=1);
+declare(strict_types=1);
 
-    require_once "Beverage.php";
-    require_once "Beer.php";
+require_once "Beverage.php";
+require_once "Beer.php";
 
-    $cola = new Beverage('black', 2.00);
-    echo "<pre>";
-        echo $cola->getInfo() ."<br>";
-        echo $cola->temperature;
-    echo "</pre>";
+$cola = new Beverage('black', 2.00);
+echo "<pre>";
+    echo $cola->getInfo() ."<br>";
+    echo $cola->getTemperature();
+echo "</pre>";
 
-    $duvel = new Beer('blond', 3.50, 'Duvel', 8.50);
-    echo "<pre>";
-    echo $duvel->alcoholPercentage ."<br>";
-    echo $duvel->getAlcoholPercentage() ."<br>";
-    echo $duvel->color ."<br>";
-    echo $duvel->getInfo() ."<br>";
-    echo $cola->getAlcoholPercentage();
-    echo "</pre>";
-?>
+$duvel = new Beer('blond', 3.50, 'Duvel', 8.50);
+echo "<pre>";
+//echo $duvel->alcoholPercentage ."<br>";
+echo $duvel->getAlcoholPercentage() ."<br>";
+echo $duvel->getColor() ."<br>";
+echo $duvel->getTemperature() ."<br>";
+echo $duvel->getInfo() ."<br>";
+//echo $cola->getAlcoholPercentage();
+echo "</pre>";
+
+$duvel->setColor("light");
+echo $duvel->getColor() ."<br>";
+echo $duvel->beerInfo();
